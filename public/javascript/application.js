cd573c47c44978ee6b67578a78ecbe21f5d6d378
Tana Jukes <tana.jukes@gmail.com>
2016-06-29 17:10:30 -0700

    Enable displaying contacts
    This commit initiates jQuery functionality to retrieve contact information from the database and display it in the view.
    - Add button to view (index.html) to 'Show Contacts'
    - Add div below button to display retrieved contacts in
    - Add jQuery event listener to 'Show Contacts' button that sends a request to the API for contact data
    - Handler function first clears any existing contact info data from the view, to avoid duplication when the button is pressed more than once
    - Handler then sends the contact data that was returned to by the API to a 'showContact' helper method that adds each contact's details to the view

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("button#show-contacts").on("click", function() {
+    $(".contacts-list").empty();
+    $.getJSON('/contacts', function (contacts) {
+      contacts.forEach(showContact);
+    });
+  });
+
+  function showContact(contact) {
+    // Should refactor to use template
+    var showName = $("<h3>").appendTo(".contacts-list");
+    showName.text(contact.first_name + " " + contact.last_name);
+    var showDetails = $("<p>").appendTo(".contacts-list");
+    showDetails.text(contact.email + " | " + contact.phone);
+  }
+
 });
