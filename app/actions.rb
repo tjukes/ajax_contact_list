0091879227ec548e06912307cfc829ab53083471
Tana Jukes <tana.jukes@gmail.com>
2016-06-29 21:26:57 -0700

    Big ugly commit
    This may or may not be functional code; I'm committing it so I have some kind of baseline. I'm fairly certain that this code worked 15 minutes ago, then I undid and redid a couple things and must ahve broken something. Now jQuery has mysteriously quit ($ is not a function, .val is not a function).
    
    Future commits on this branched will be untested, unless I figure out how to convince jQuery to give me another chance.

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,14 @@ end
 get '/contacts' do
   Contact.all.to_json
 end
+
+post '/contacts/create' do
+  results = {result: false}
+  contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone: params[:phone])
+  if contact.save
+    results[:result] = true
+    results[:contact_id] = contact.id
+    # Should refactor to add error messages here if save unsuccessful
+  end
+  results.to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  // Show contacts
+
   $("button#show-contacts").on("click", function() {
     $(".contacts-list").empty();
     $.getJSON('/contacts', function (contacts) {
@@ -14,5 +16,30 @@ $(document).ready(function() {
     var showDetails = $("<p>").appendTo(".contacts-list");
     showDetails.text(contact.email + " | " + contact.phone);
   }
+
+  // Add contacts
+
+  $(".contact-form").on("submit", function(evt) {
+    // Prevent anything from acutally being submitted
+    evt.preventDefault();
+    // Get contact information from input fields
+    var first_name = $("#first-name").val();
+    var last_name = $("#last-name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    // If all information is present, use it to create new contact
+    if (first_name && last_name && email && phone) {
+        $.post('/contacts/create',
+        {first_name: first_name, last_name: last_name, email: email, phone: phone},
+        function(results) {
+          var showMessage = $("<p>").appendTo(".message");
+          if (results["result"]) { showMessage.text("Contact saved."); }
+        }, "json" );
+    } else {
+      showMessage.text("Contact was not saved.");
+    }
+    // Clear form fields
+    $(".contact-form input").val("");
+  });
 
 });
